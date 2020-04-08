488b5559b6753c242e1deced1391b88bc18ad1e6
natalia <nata2@uw.edu>
2020-04-08 09:58:38 -0700

    rigid body for monsters

---
+++ projectStart/Assets/scripts/DestroyAfter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+       
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        Destroy(gameObject);
+    }
+}
