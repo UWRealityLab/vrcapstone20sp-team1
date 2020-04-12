bfe879ec0db793ed1740e121b42b8902b2cd683b
Marc <mma2448@cs.washington.edu>
2020-04-12 15:21:17 -0700

    Added functionality that highlights the enemey closest to camera's center. Right now done with boxes; Added dojo, no textures. Will be corrected byh graphic designers; Minor Website edits

---
+++ projectStart/Assets/Assets/Marc's Workspace/Code/HighlightEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightEnemy : MonoBehaviour
+{
+
+    public Camera cam;
+    GameObject highlighted = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] enemies;
+
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+        GameObject nearestObj = null;
+        float nearestDist = float.MaxValue;
+
+        foreach (GameObject enemy in enemies)
+        {
+            Vector3 pos = cam.WorldToViewportPoint(enemy.transform.position);
+            float dist = Mathf.Sqrt( Mathf.Pow(Mathf.Abs(pos.x) - 0.5f, 2) + Mathf.Pow(Mathf.Abs(pos.y) - 0.5f, 2));
+
+            if(nearestDist.CompareTo(dist) > 0)
+            {
+                nearestDist = dist;
+                nearestObj = enemy;
+            }
+        }
+
+        if (highlighted != nearestObj)
+        {
+            if (highlighted != null)
+                highlighted.GetComponentInChildren<ParticleSystem>().Stop();
+            if (nearestObj != null)
+                nearestObj.GetComponentInChildren<ParticleSystem>().Play();
+
+            highlighted = nearestObj;
+        }
+
+    }
+
+
+}
---
+++ projectStart/Assets/Assets/Marc's Workspace/Code/ThrowNinjaStarVR.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ThrowNinjaStarVR : MonoBehaviour
+{
+    public GameObject starObject;
+    public GameObject starSpawn;
+    public Hand hand;
+
+    private GameObject star;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hand.GetGrabStarting() == GrabTypes.Pinch)
+        {
+            hand.TriggerHapticPulse(2000); // doesn't work yet
+            SpawnStar();
+        } 
+        else if (hand.GetGrabEnding() == GrabTypes.Pinch) 
+        {
+            Throw();
+            hand.TriggerHapticPulse(2000); // doesn't work yet
+        }
+    }
+
+    private void Throw()
+    {
+        star = null;
+    }
+
+    private void SpawnStar()
+    {
+        star = Instantiate(starObject, starSpawn.transform.position, starSpawn.transform.rotation) as GameObject;
+
+        hand.AttachObject(star, GrabTypes.Pinch);
+    }
+}
