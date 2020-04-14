d87fa5711945fc4f7931a5becc53d5b13a4ed698
Marc <mma2448@cs.washington.edu>
2020-04-14 14:00:57 -0700

    new target system; modified the background

---
+++ projectStart/Assets/Assets/Marc's Workspace/Code/RailingTarget.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RailingTarget : MonoBehaviour
+{
+    public GameObject destructable;
+    public Rigidbody anchor;
+
+    public Func<bool> destroyConditionFunc { get; set; } = null;
+    public Func<Vector3> velocityFunc {get; set;} = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(velocityFunc != null)
+            anchor.velocity = velocityFunc();
+
+        if (destroyConditionFunc != null && destroyConditionFunc())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public GameObject GetDestructable()
+    {
+        return destructable;
+    }
+
+    public Rigidbody GetAnchor()
+    {
+        return anchor;
+    }
+}
