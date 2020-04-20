26c25afe96b83d4ca1e1da31280a46626c2f9649
natalia <nata2@uw.edu>
2020-04-20 16:39:21 -0700

    light

---
+++ projectStart/Assets/scripts/RewardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardController : MonoBehaviour
+{
+    public float speed = 0.2f;
+    //public final position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = new Vector3(0, 1, 0); //calculate position of the player
+        if(transform.position.y < 2)
+        {
+            Debug.Log(transform.position);
+            transform.Translate(v * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ projectStart/Assets/scripts/RewardLightController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardLightController : MonoBehaviour
+{
+    public Light lt;
+    public float minIntensity = 3.0f;
+    public float maxIntensity = 2.7f;
+    GameManager manager;
+
+    float random;
+
+    void Start()
+    {
+        manager = GameManager.GetInstance();
+        lt = GetComponent<Light>();
+        random = Random.Range(0.0f, 100.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        lt.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+}
