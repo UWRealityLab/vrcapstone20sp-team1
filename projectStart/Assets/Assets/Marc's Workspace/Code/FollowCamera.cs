a0d54379905d2c6c8035d0f92c99b7e4d161ec09
Marc <mma2448@cs.washington.edu>
2020-04-27 04:27:06 -0700

    adding rough wisp movement

---
+++ projectStart/Assets/Assets/Marc's Workspace/Code/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Camera playerCamera;
+
+
+    private Vector3 velocity = Vector3.zero;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Transform camera = playerCamera.GetComponent<Transform>();
+
+        Debug.Log(camera.forward);
+
+        Vector3 loc = playerPosition.position + Vector3.Normalize(camera.forward + (camera.up * 0.8f) + (camera.right * 0.5f)) * 4;
+
+        transform.position = Vector3.SmoothDamp(transform.position,
+                                                loc,
+                                                ref velocity,
+                                                1f,
+                                                2);
+    }
+}
