1d637ecdcaf9103800488f64d5bdf5dc60b5b359
mattwcook <32008216+mattwcook@users.noreply.github.com>
2020-04-28 10:01:11 -0700

    Created Matt's Assets
    Matt's test scene, grandpa mist, tileable main room segment, and custom material

---
+++ projectStart/Assets/Matt's Assets/MistMotion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MistMotion : MonoBehaviour
+{
+    Vector3 currentPosition;
+    Vector3 previousPostion;
+    ParticleSystem particles;
+    bool wasStill = false;
+    float stillTimer;
+    float timeToRemainStill = .10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        previousPostion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position;
+        if (currentPosition != previousPostion && wasStill == true)
+        {
+            wasStill = false;
+            particles.Clear();
+            //particles.startLifetime = .5f;
+            Debug.Log("Began Motion");
+        }
+        if (currentPosition == previousPostion)
+        {
+            stillTimer += Time.deltaTime;
+            if (stillTimer >= timeToRemainStill)
+            {
+                wasStill = true;
+                particles.startLifetime = 3.0f;
+                //particles.emission.rateOverTime = 8f;
+            }
+            
+        }
+        else
+        {
+            stillTimer = 0;
+            particles.startLifetime = .5f;
+        }
+        previousPostion = transform.position;
+    }
+}
