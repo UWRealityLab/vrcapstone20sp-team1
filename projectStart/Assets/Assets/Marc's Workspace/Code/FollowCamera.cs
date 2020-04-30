fd3a07a2894a2d918fc380647bee1fbc35ffc5bf
Marc <mma2448@cs.washington.edu>
2020-04-30 08:03:05 -0700

    Wisp movement changes; add sample hover behavior

--- projectStart/Assets/Assets/Marc's Workspace/Code/FollowCamera.cs
+++ projectStart/Assets/Assets/Marc's Workspace/Code/FollowCamera.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FollowCamera : MonoBehaviour
 {
     public Transform playerPosition;
     public Camera playerCamera;
 
+    public GameObject sampleObject;
+
+    public float followSpeed = 0.1f;
+    public float maxSpeed = 10;
+
 
     private Vector3 velocity = Vector3.zero;
+    private Vector3 hoverPos;
+    private float hoverRadius;
     void Start()
     {
-        
+        hoverPos = sampleObject.GetComponent<Transform>().position + new Vector3(0, sampleObject.GetComponent<Renderer>().bounds.size.y / 2 + 0.75f, 0);
+        hoverRadius = (sampleObject.GetComponent<Renderer>().bounds.size.x / 2) + 0.25f;
     }
 
     // Update is called once per frame
@@ -19,15 +28,31 @@ public class FollowCamera : MonoBehaviour
     {
 
         Transform camera = playerCamera.GetComponent<Transform>();
+        Vector3 loc;
 
-        Debug.Log(camera.forward);
-
-        Vector3 loc = playerPosition.position + Vector3.Normalize(camera.forward + (camera.up * 0.8f) + (camera.right * 0.5f)) * 4;
-
-        transform.position = Vector3.SmoothDamp(transform.position,
+        if(!Input.GetKey(KeyCode.Space))
+        {
+            loc = playerPosition.position + Vector3.Normalize(camera.forward + (camera.up * 0.65f) + (camera.right * 0.5f)) * 4;
+            transform.position = Vector3.SmoothDamp(transform.position,
                                                 loc,
                                                 ref velocity,
-                                                1f,
-                                                2);
+                                                followSpeed,
+                                                maxSpeed);
+        } else
+        {
+            if((hoverPos - this.transform.position).magnitude > hoverRadius)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position,
+                                                hoverPos,
+                                                ref velocity,
+                                                followSpeed,
+                                                maxSpeed);
+            } else
+            {
+                transform.RotateAround(hoverPos, Vector3.up, 360 * Time.deltaTime);
+            }
+        }
+
+        
     }
 }
---
+++ projectStart/Assets/Assets/Marc's Workspace/Code/WispMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispMovement : MonoBehaviour
+{
+
+    public float floatStrength = 1;
+    public float floatSpeed = 1;
+
+    private float sinedValue;
+    private Vector3 prevAnchorPos;
+
+    void Start()
+    {
+        sinedValue = Time.time;
+        prevAnchorPos = GetComponentInParent<Transform>().position;
+    }
+
+    void Update()
+    {
+        Vector3 deltaPos = (GetComponentInParent<Transform>().position - prevAnchorPos);
+        float parentSpeed = Mathf.Sqrt(deltaPos.x * deltaPos.x + deltaPos.z * deltaPos.z);
+        sinedValue += Time.deltaTime * (2 + (parentSpeed * 500));
+
+        Debug.Log((2 + (parentSpeed * 500)));
+
+        transform.position = new Vector3(transform.position.x,
+            GetComponentInParent<Transform>().position.y + ((float)Mathf.Sin(sinedValue) * floatStrength),
+            transform.position.z);
+
+
+        prevAnchorPos = GetComponentInParent<Transform>().position;
+    }
+}
