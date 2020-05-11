a6114a7e8f429852436e5c77a37308e5df13b429
mattwcook <32008216+mattwcook@users.noreply.github.com>
2020-05-11 14:29:25 -0700

    Added Shrine and components

---
+++ projectStart/Assets/Matt's Assets/EmeraldHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmeraldHover : MonoBehaviour
+{
+    Vector3 startPosition;
+    Vector3 position;
+    public float amplitude = .01f;
+    public float frequency = .2f;
+    float time;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per framess
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.position = startPosition + new Vector3(0, 1, 0) * amplitude * Mathf.Sin(360 * frequency * time * Mathf.PI / 180);
+    }
+}
