25a879c3390e52e886ef321539c5edfd9169710d
Marc <mma2448@cs.washington.edu>
2020-05-23 11:02:02 -0700

    crashing through wall

---
+++ projectStart/Assets/Dragon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animation anim;
+    void Start()
+    {
+        anim.Play("birth");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!anim.isPlaying)
+        {
+            anim.Play("attack2");
+        } 
+    }
+}
