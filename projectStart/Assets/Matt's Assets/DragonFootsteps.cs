fdbb811993d8f66f216466cc21d53ec177b49dc8
mattwcook <32008216+mattwcook@users.noreply.github.com>
2020-11-17 17:05:49 -0800

    Added footstep sounds to dragon

---
+++ projectStart/Assets/Matt's Assets/DragonFootsteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DragonFootsteps : MonoBehaviour
+{
+    public AudioSource rightFoot;
+    public AudioSource leftFoot;
+    public AudioClip footstep1;
+    public AudioClip footstep2;
+
+ 
+
+    void Thud1()
+    {
+        rightFoot.PlayOneShot(footstep1);
+    }
+    void Thud2()
+    {
+        leftFoot.PlayOneShot(footstep2);
+    }
+}
