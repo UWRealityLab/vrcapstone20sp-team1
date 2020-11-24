f34f272cbff3d69abda3ed6ee3f4660efbe0e04b
mattwcook <32008216+mattwcook@users.noreply.github.com>
2020-11-23 21:55:39 -0800

    Fixed Dragon Animations
    Elimated Legacy Animation component and replaced with newly created animation controller. Fixed animations to no longer be Legacy in Rig Type. Adjusted Dragon script to work with the animation controller

--- projectStart/Assets/Dragon.cs
+++ projectStart/Assets/Dragon.cs
@@ -5,45 +5,65 @@ using UnityEngine;
 public class Dragon : Monster
 {
     // Start is called before the first frame update
-    public Animation anim;
+    ////////////public Animation anim;
     public AudioClip roar;
     public AudioClip death;
+    public AudioClip yelp;
+    float destroyDelay = 3;
 
     private bool isDying = false;
     void Start()
     {
-        anim.Play("birth");
+        //////////anim.Play("birth");
         this.GetComponent<AudioSource>().PlayOneShot(roar);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (!anim.isPlaying)
         {
             if (isDying)
             {
                 Destroy(this.gameObject);
             }
-            anim.Play("Take 001");
+            anim.Play("attack2");
             GetComponent<AudioSource>().PlayOneShot(roar);
+        }
+        */
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {
+            StartCoroutine(DestroyDelay(destroyDelay));
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            animator.SetTrigger("Attack");
         }
     }
 
     public override void Death()
     {
-        anim.Play("die");
+        //////////anim.Play("die");
         GetComponent<AudioSource>().PlayOneShot(death);
-        isDying = true;
+        ///////isDying = true;
+        animator.SetBool("isDying", true);
     }
     public override void HitReaction()
     {
-        Debug.Log("hit");
+        ////////////Debug.Log("hit");
+        animator.SetTrigger("Flinch");
     }
 
     public override void PlayHitAudio()
     {
         //GetComponent<AudioSource>().PlayOneShot(roar);
+        GetComponent<AudioSource>().PlayOneShot(yelp);
+    }
+    private IEnumerator DestroyDelay(float t)
+    {
+        yield return new WaitForSeconds(t);
+        Destroy(gameObject);
     }
 
 }
