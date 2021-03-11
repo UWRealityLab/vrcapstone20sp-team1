7794a433f4b072ff0cccb154ccc6742dff3a459a
mattwcook <32008216+mattwcook@users.noreply.github.com>
2021-03-11 15:22:33 -0800

    New System for Pointing Backward

---
+++ projectStart/Assets/Matt's Assets/PointSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+
+    public Transform playerHead;
+    RaycastHit hit;
+    int layerMask;
+    float timer = 0;
+    float timeLimit = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = 1 << 8;
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (Physics.Raycast(playerHead.position, playerHead.TransformDirection(Vector3.forward), out hit, 25f, layerMask))
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeLimit)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+}
