4c03da96aa17faa8a03cb6a6f85dfb4a93d97f63
mattwcook <32008216+mattwcook@users.noreply.github.com>
2021-04-29 14:30:38 -0700

    Swipe Visual Cue

--- projectStart/Assets/AirSlash.cs
+++ projectStart/Assets/AirSlash.cs
@@ -6,19 +6,23 @@ public class AirSlash : MonoBehaviour, Weapon
 {
 
     public int damageMultiplier = 5;
-    public float duration = 2.5f;
+    public float range = 25;
+    float duration;
+    float destroyDelay = .1f;
     
     private float spawnTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnTime = Time.time;    
+        spawnTime = Time.time;
+        duration = range / GetComponent<Rigidbody>().velocity.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
         if(Time.time - spawnTime >= duration)
         {
             Destroy(this.gameObject);
@@ -29,11 +33,17 @@ public class AirSlash : MonoBehaviour, Weapon
     {
         //Debug.Log("MARC DEBUG LINE: Triggered");
         //this.GetComponent<Collider>().enabled = false;
+        StartCoroutine(DestroySelf());
     }
 
     public int damage()
     {
         Vector3 velocity = gameObject.GetComponent<Rigidbody>().velocity;
         return Mathf.RoundToInt(velocity.magnitude) * damageMultiplier;
+    }
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
     }
 }
---
+++ projectStart/Assets/Matt's Assets/SwipeCuePosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeCuePosition : MonoBehaviour
+{
+    float playerHeight;
+    float offset;
+    float forearmLength;
+    float shoulderDistance;
+    Transform shoulder;
+    Transform elbow;
+    Transform wrist;
+    //public Transform head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHeight = transform.parent.localPosition.y;
+        offset = playerHeight * .25f;
+        forearmLength = playerHeight * .156f;
+        shoulderDistance = playerHeight / 8;
+        shoulder = transform.GetChild(0);
+        elbow = shoulder.transform.GetChild(0);
+        wrist = shoulder.transform.GetChild(0);
+        transform.localPosition = new Vector3(-shoulderDistance, 0, 0);
+        elbow.transform.localPosition = new Vector3(0, 0, forearmLength);
+        wrist.transform.localPosition = new Vector3(0, 0, forearmLength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(0, -offset, 0);
+        transform.eulerAngles = new Vector3(0, transform.parent.eulerAngles.y, transform.parent.eulerAngles.z);
+    }
+}
