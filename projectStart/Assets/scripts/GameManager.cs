025986309bfd1871b33689e7f0e09d22abe6f80f
mattwcook <32008216+mattwcook@users.noreply.github.com>
2021-08-29 17:19:52 -0700

    Fixed null reference

--- projectStart/Assets/scripts/GameManager.cs
+++ projectStart/Assets/scripts/GameManager.cs
@@ -291,6 +291,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         grandpa = Grandpa.GetInstance();
         audioManager = AudioManager.GetInstance();
         audioManager.PlayIntro();
